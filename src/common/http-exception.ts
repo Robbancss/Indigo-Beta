f9acaed4bef8d9db1f0e7fb801a470011f5dc355
robert.panczel <robi.panczel@gmail.com>
2020-09-23 01:47:37 +0200

    Quick fix on the faulty constructor shorthand

--- src/common/http-exception.ts
+++ src/common/http-exception.ts
@@ -2,6 +2,9 @@
  * Helper class for encapsulating errors related to HTTP requests
  */
 export default class HttpException extends Error {
+  statusCode: number;
+  message: string;
+  error: string | null;
   /**
    * @param {number} statusCode Error status code
    * @param {string} message Error message
@@ -9,5 +12,8 @@ export default class HttpException extends Error {
    */
   constructor(statusCode: number, message: string, error?: string) {
     super(message);
+    this.statusCode = statusCode;
+    this.message = message;
+    this.error = error || null;
   }
 }
